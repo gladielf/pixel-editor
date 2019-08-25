b7dd946545d3b8f0c8f1ba7c135ec7e1e7345aa3
Fermin conejo <gladielf@gmail.com>
2019-08-25 18:34:55 +0200

    Created pe-pixel component

---
+++ src/components/pe-pixel.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+    .pe-pixel
+</template>
+
+<script>
+export default {
+  name: 'pe-pixel',
+  props: {
+    pixelSide: {
+      type: Number,
+      default: 1
+    }
+  },
+  watch: {
+    pixelSide () {
+      this.setPixelSide()
+    }
+  },
+  methods: {
+    setPixelSide () {
+      this.$el.style.setProperty('--pixel-side', `${this.pixelSide}em`)
+    }
+  },
+  mounted () {
+    this.setPixelSide()
+  }
+}
+</script>
+
+<style lang="sass">
+    .pe-pixel
+        height: var(--pixel-side)
+        width: var(--pixel-side)
+        box-shadow: inset 0 0 0 1px black
+</style>
